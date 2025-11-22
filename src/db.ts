17805ccba17811f7a2873bf193409ee3588567cd
zhiyulin <u10111012@gmail.com>
2025-11-22 21:02:06 +0800

    setting up db

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from './generated/prisma/client.js'
+import { neon } from '@neondatabase/serverless'
 
 import { PrismaPg } from '@prisma/adapter-pg'
+
+export async function getClient() {
+  const databaseUrl = process.env.DATABASE_URL
+  if (!databaseUrl) return undefined
+  return neon(databaseUrl)
+}
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
